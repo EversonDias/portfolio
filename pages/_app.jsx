94678eabdfb4caf3787a506ddcdc68a0a66dfd96
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-02 22:36:57 -0300

    alpha fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,11 +4,15 @@ import './styles.css'
 import { Header } from "../src/patterns/header";
 import Footer from "../src/patterns/footer";
 
+
 export default function MyApp(props) {
+    //adicionar um callback para trocar o tema sempre que for trocado no sistema.
+    
+
     return (
         <>
-            <Header/>
-            <props.Component {...props.pageProps}/>
+            <Header />
+            <props.Component {...props.pageProps} />
             <Footer />
         </>
     )
--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,20 +1,26 @@
-/*import de modules react */
-import { useContext } from "react"
-
-/*import de modues styled-components */
-import { ThemeContext } from "styled-components"
-
-/*import do botão de traca de tema */
 import Link from "next/link"
 import styles from './styles.module.css';
+import { MdOutlineLightMode, MdDarkMode } from 'react-icons/md'
 
 export default function NavBar() {
+    let theme;
+    const handleTheme = () => {
+        if (theme) {
+            theme = false
+            document.body.setAttribute('class', 'dark')
+        } else {
+            theme = true
+            document.body.setAttribute('class', 'light')
+        }
+    }    
+
     return (
         <nav className={styles.nav}>
             <Link href="/projects"><a className={styles.a}>Projects</a></Link>
             <Link href="/about"><a className={styles.a}>About</a></Link>
 
             {/* <Link href="/about">Contact</Link> */}
+            <MdDarkMode className={styles.icon} onClick={handleTheme} />
         </nav>
 
     )
